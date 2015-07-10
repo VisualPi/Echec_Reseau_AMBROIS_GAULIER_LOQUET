71c758759c3451be560ae3e57e625f2d62d4bb6c
VisualPi <gintakun2@yahoo.fr>
2015-07-10 22:25:32 +0200

    Debut de projet (GUI + sprites) j'ai enlever le sfml static ça faisait du caca :)

--- Project-Echec/main.cpp
+++ Project-Echec/main.cpp
@@ -1,88 +1,243 @@
+#include <SFML/Graphics.hpp>
+#include <SFML\Window.hpp>
 #include <SFGUI/SFGUI.hpp>
 #include <SFGUI/Widgets.hpp>
 
-#include <SFML/Graphics.hpp>
-
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
+sf::Texture pawnW, queenW, kingW, bishopW, knightW, rookW, pawnB, queenB, kingB, bishopB, knightB, rookB, damier;
+sf::Sprite spPawnW, spQueenW, spKingW, spBishopW, spKnightW, spRookW, spPawnB, spQueenB, spKingB, spBishopB, spKnightB, spRookB, spDamier;
+std::vector<sf::Sprite> whitePawn;
+std::vector<sf::Sprite> blackPawn;
+std::vector<sf::Sprite> whiteRook;
+std::vector<sf::Sprite> blackRook;
+std::vector<sf::Sprite> whiteKnight;
+std::vector<sf::Sprite> blackKnight;
+std::vector<sf::Sprite> whiteBishop;
+std::vector<sf::Sprite> blackBishop;
+sf::Vector2f damierSize = sf::Vector2f(600, 600);
 
-class HelloWorld {
-public:
-	// Our button click handler.
-	void OnButtonClick();
 
-	void Run();
+void OnButtonLocalClick();
+void OnButtonComputerClick();
+void OnButtonLigneClick();
+void OnButtonOptionClick();
+void OnButtonQuitClick();
+void drawAllPieces();
 
-private:
-	// Create an SFGUI. This is required before doing anything with SFGUI.
-	sfg::SFGUI m_sfgui;
+sf::RenderWindow render_window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Hello world!", sf::Style::Titlebar);
 
-	// Create the label pointer here to reach it from OnButtonClick().
-	sfg::Label::Ptr m_label;
-};
+int main()
+{
 
-void HelloWorld::OnButtonClick() {
-	m_label->SetText("Hello SFGUI, pleased to meet you!");
-}
+#pragma region GUI
+	sfg::SFGUI sfgui;
 
-void HelloWorld::Run() {
-	// Create SFML's window.
-	sf::RenderWindow render_window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Hello world!");
+	//auto label = sfg::Label::Create("PUTAIN DE GUI QSLKDJSQKLJ");
+	auto buttonLocal = sfg::Button::Create("1v1 local");
+	auto buttonLigne = sfg::Button::Create("1v1 en ligne");
+	auto buttonComputer = sfg::Button::Create("1 vs computer");
+	auto buttonOption = sfg::Button::Create("Options");
+	auto buttonQuitter = sfg::Button::Create("Quitter");
 
-	// Create the label.
-	m_label = sfg::Label::Create("Hello world!");
+	buttonLocal->GetSignal(sfg::Widget::OnLeftClick).Connect(OnButtonLocalClick);
+	buttonComputer->GetSignal(sfg::Widget::OnLeftClick).Connect(OnButtonComputerClick);
+	buttonLigne->GetSignal(sfg::Widget::OnLeftClick).Connect(OnButtonLigneClick);
+	buttonOption->GetSignal(sfg::Widget::OnLeftClick).Connect(OnButtonOptionClick);
+	buttonQuitter->GetSignal(sfg::Widget::OnLeftClick).Connect(OnButtonQuitClick);
 
-	// Create a simple button and connect the click signal.
-	auto button = sfg::Button::Create("Greet SFGUI!");
-	button->GetSignal(sfg::Widget::OnLeftClick).Connect(std::bind(&HelloWorld::OnButtonClick, this));
-
-	// Create a vertical box layouter with 5 pixels spacing and add the label
-	// and button to it.
 	auto box = sfg::Box::Create(sfg::Box::Orientation::VERTICAL, 5.0f);
-	box->Pack(m_label);
-	box->Pack(button, false);
+	//box->Pack(label);
+	box->Pack(buttonLigne);
+	box->Pack(buttonLocal);
+	box->Pack(buttonComputer);
+	box->Pack(buttonOption);
+	box->Pack(buttonQuitter);
+	//box->Pack(button, false);
 
-	// Create a window and add the box layouter to it. Also set the window's title.
 	auto window = sfg::Window::Create();
-	window->SetTitle("Hello world!");
+	window->SetTitle("Menu");
 	window->Add(box);
+	window->SetRequisition(sf::Vector2f(SCREEN_WIDTH / 4, SCREEN_HEIGHT));
+#pragma endregion DECLARATION DE LA GUI
 
-	// Create a desktop and add the window to it.
-	sfg::Desktop desktop;
-	desktop.Add(window);
+#pragma region SPRITES
+	pawnW.loadFromFile("..\\Ressources\\PawnWhite.png");		pawnW.setSmooth(true);
+	queenW.loadFromFile("..\\Ressources\\QueenWhite.png");		queenW.setSmooth(true);
+	kingW.loadFromFile("..\\Ressources\\KingWhite.png");		kingW.setSmooth(true);
+	bishopW.loadFromFile("..\\Ressources\\BishopWhite.png");	bishopW.setSmooth(true);
+	knightW.loadFromFile("..\\Ressources\\KnightWhite.png");	knightW.setSmooth(true);
+	rookW.loadFromFile("..\\Ressources\\RookWhite.png");		rookW.setSmooth(true);
 
-	// We're not using SFML to render anything in this program, so reset OpenGL
-	// states. Otherwise we wouldn't see anything.
+	pawnB.loadFromFile("..\\Ressources\\PawnBlack.png");		pawnB.setSmooth(true);
+	queenB.loadFromFile("..\\Ressources\\QueenBlack.png");		queenB.setSmooth(true);
+	kingB.loadFromFile("..\\Ressources\\KingBlack.png");		kingB.setSmooth(true);
+	bishopB.loadFromFile("..\\Ressources\\BishopBlack.png");	bishopB.setSmooth(true);
+	knightB.loadFromFile("..\\Ressources\\KnightBlack.png");	knightB.setSmooth(true);
+	damier.loadFromFile("..\\Ressources\\damier.png");			damier.setSmooth(true);
+	rookB.loadFromFile("..\\Ressources\\RookBlack.png");		rookB.setSmooth(true);
+
+	spPawnW.setTexture(pawnW);		spPawnW.setScale(sf::Vector2f(.3125f, .3125f));
+	spQueenW.setTexture(queenW);	spQueenW.setScale(sf::Vector2f(.3125f, .3125f));
+	spKingW.setTexture(kingW);		spKingW.setScale(sf::Vector2f(.3125f, .3125f));
+	spBishopW.setTexture(bishopW);	spBishopW.setScale(sf::Vector2f(.3125f, .3125f));
+	spKnightW.setTexture(knightW);	spKnightW.setScale(sf::Vector2f(.3125f, .3125f));
+	spRookW.setTexture(rookW);		spRookW.setScale(sf::Vector2f(.3125f, .3125f));
+	spPawnB.setTexture(pawnB);		spPawnB.setScale(sf::Vector2f(.3125f, .3125f));
+	spQueenB.setTexture(queenB);	spQueenB.setScale(sf::Vector2f(.3125f, .3125f));
+	spKingB.setTexture(kingB);		spKingB.setScale(sf::Vector2f(.3125f, .3125f));
+	spBishopB.setTexture(bishopB);	spBishopB.setScale(sf::Vector2f(.3125f, .3125f));
+	spKnightB.setTexture(knightB);	spKnightB.setScale(sf::Vector2f(.3125f, .3125f));
+	spRookB.setTexture(rookB);		spRookB.setScale(sf::Vector2f(.3125f, .3125f));
+
+	spDamier.setTexture(damier);	spDamier.setScale(sf::Vector2f(.5859375f, .5859375f)); // damier de 1024*1024 dans 600*600 donc x et y = 600/1024
+
+	spDamier.setPosition(SCREEN_WIDTH - damierSize.x, 0);
+	
+	for (int i = 0; i < 8; ++i)
+	{
+		whitePawn.push_back(sf::Sprite(spPawnW));
+		blackPawn.push_back(sf::Sprite(spPawnB));
+	}
+	for (int i = 0; i < 2; ++i)
+	{
+		whiteRook.push_back(sf::Sprite(spRookW));
+		blackRook.push_back(sf::Sprite(spRookB));
+		whiteKnight.push_back(sf::Sprite(spKnightW));
+		blackKnight.push_back(sf::Sprite(spKnightB));
+		whiteBishop.push_back(sf::Sprite(spBishopW));
+		blackBishop.push_back(sf::Sprite(spBishopB));
+	}
+
+#pragma endregion DECLARATION DES TEXTURES ET SPRITES
+
+
+
+#pragma region RENDU
 	render_window.resetGLStates();
-
-	// Main loop!
-	sf::Event event;
 	sf::Clock clock;
-
-	while (render_window.isOpen()) {
+	while (render_window.isOpen())
+	{
 		// Event processing.
-		while (render_window.pollEvent(event)) {
-			desktop.HandleEvent(event);
-
-			// If window is about to be closed, leave program.
-			if (event.type == sf::Event::Closed) {
+		sf::Event event;
+		while (render_window.pollEvent(event))
+		{
+			window->HandleEvent(event);
+			switch (event.type)
+			{
+			case sf::Event::Closed:
 				render_window.close();
+				break;
+			default:
+				break;
 			}
 		}
-
-		// Update SFGUI with elapsed seconds since last call.
-		desktop.Update(clock.restart().asSeconds());
-
-		// Rendering.
-		render_window.clear();
-		m_sfgui.Display(render_window);
+		window->Update(clock.restart().asSeconds());
+		render_window.clear(sf::Color::Blue);
+		render_window.draw(spDamier);
+		drawAllPieces();
+		sfgui.Display(render_window);
 		render_window.display();
 	}
-}
-
-int main() {
-	HelloWorld hello_world;
-	hello_world.Run();
+#pragma endregion BOUCLE DE RENDU
 
 	return 0;
 }
+
+void drawAllPieces()
+{
+	render_window.draw(blackRook.at(0));
+	render_window.draw(blackRook.at(1));
+
+	render_window.draw(blackKnight.at(0));
+	render_window.draw(blackKnight.at(1));
+
+	render_window.draw(blackBishop.at(0));
+	render_window.draw(blackBishop.at(1));
+
+	render_window.draw(spQueenB);
+	render_window.draw(spKingB);
+
+	for (int i = 0; i < 8; ++i)
+	{
+		render_window.draw(blackPawn.at(i));
+	}
+	//-----------------------------------------//
+	render_window.draw(whiteRook.at(0));
+	render_window.draw(whiteRook.at(1));
+
+	render_window.draw(whiteKnight.at(0));
+	render_window.draw(whiteKnight.at(1));
+
+	render_window.draw(whiteBishop.at(0));
+	render_window.draw(whiteBishop.at(1));
+
+	render_window.draw(spQueenW);
+	render_window.draw(spKingW);
+
+	for (int i = 0; i < 8; ++i)
+	{
+		render_window.draw(whitePawn.at(i));
+	}
+}
+void preparePlateau()
+{
+	blackRook.at(0).setPosition(200, 0);
+	blackRook.at(1).setPosition(725, 0);
+
+	blackKnight.at(0).setPosition(275, 0);
+	blackKnight.at(1).setPosition(650, 0);
+
+	blackBishop.at(0).setPosition(350, 0);
+	blackBishop.at(1).setPosition(575, 0);
+
+	spKingB.setPosition(425,0);
+	spQueenB.setPosition(500,0);
+
+	for (int i = 0; i < 8; ++i)
+	{
+		blackPawn.at(i).setPosition(200 + (i * 75),75);
+	}
+	//--------------------------------------------------//
+	whiteRook.at(0).setPosition(200, damierSize.y - 75);
+	whiteRook.at(1).setPosition(725, damierSize.y - 75);
+
+	whiteKnight.at(0).setPosition(275, damierSize.y - 75);
+	whiteKnight.at(1).setPosition(650, damierSize.y - 75);
+
+	whiteBishop.at(0).setPosition(350, damierSize.y - 75);
+	whiteBishop.at(1).setPosition(575, damierSize.y - 75);
+
+	spKingW.setPosition(425, damierSize.y - 75);
+	spQueenW.setPosition(500, damierSize.y - 75);
+
+	for (int i = 0; i < 8; ++i)
+	{
+		whitePawn.at(i).setPosition(200 + (i * 75), damierSize.y - 150);
+	}
+}
+void OnButtonLocalClick()
+{
+	preparePlateau();
+	std::cout << "Solo game " << std::endl;
+}
+void OnButtonComputerClick()
+{
+	preparePlateau();
+	std::cout << "Computer game " << std::endl;
+
+}
+void OnButtonLigneClick()
+{
+	preparePlateau();
+	std::cout << "Online Game" << std::endl;
+}
+void OnButtonOptionClick()
+{
+	std::cout << "Options" << std::endl;
+}
+void OnButtonQuitClick()
+{
+	render_window.close();
+}
+
